20f70253e06524e8ae5633adb97aa1bf6aa3bea0
EduardoDev7 <134420076+EduardoDev7@users.noreply.github.com>
2025-04-18 15:00:46 -0300

    att

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
 onload = () => {
     document.body.classList.remove("container");
-    // Faz a cartinha aparecer com fade-in
-    setTimeout(() => {
-        document.querySelector('.love-letter').style.opacity = '1';
-    }, 1000);
 };
